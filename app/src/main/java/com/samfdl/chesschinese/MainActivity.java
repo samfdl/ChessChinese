69fb67a0b179180899db2a06d15a802b821d9fb8
冯德林 <samfdl@163.com>
2018-10-26 11:37:28 +0800

    优化界面，替换腾讯天天象棋的背景

--- app/src/main/java/com/samfdl/chesschinese/MainActivity.java
+++ app/src/main/java/com/samfdl/chesschinese/MainActivity.java
@@ -3,6 +3,7 @@ package com.samfdl.chesschinese;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -55,7 +56,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         chessBoard = findViewById(R.id.chess_board);
